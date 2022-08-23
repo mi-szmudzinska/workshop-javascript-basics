0bed145ba88e3c865eb2c4941364a0f73eb1fd6f
Milena Szmudzinska <msa45@3ds.com>
2022-08-23 10:55:50 +0200

    Added comments

--- gallery/main.js
+++ gallery/main.js
@@ -1,28 +1,36 @@
-const images = document.querySelectorAll('img');
-console.log(images);
+// 1. Nasłuchujemy na zdarzenie kliknięcie w obrazki
+// 2. Pobieramy aktualny URL do klikniętego obrazka
+// 3. Zmieniamy URL na docelowy (większa rozdzielczość obrazka)
+// 4. Renderujemy duży obrazek pod listą miniaturek
 
-const bigPhotoContainer = document.querySelector('#big-photo');
+const images = document.querySelectorAll("img");
+// console.log(images);
+
+const bigPhotoContainer = document.querySelector("#big-photo");
 
 function renderPhoto(photo) {
-    //Usunięcie pierwszego dziecka z kontenera
-    bigPhotoContainer?.firstElementChild?.remove();
-    //Renderowanie obrazka 'photo' w elemncie 'bigPhotoContainer'\
-    bigPhotoContainer?.append(photo);
+  // Usunięcie pierwszego elementu (dziecka) z kontenera
+  bigPhotoContainer?.firstElementChild?.remove();
+  // Renderowanie obrazka `photo` w elemencie `bigPhotoContainer`
+  bigPhotoContainer?.append(photo);
 }
 
 function handleClickOnImage() {
-    //Pobranie atrybutu src z elementu 
-    const currentUrl = this.src;
-    //Zamiana rozdzielczosci obrazka
-    const bigPhotoUrl = currentUrl.replace('300', '900').replace('200', '600');
-    console.log(bigPhotoUrl)
-    
-    const photo = document.createElement('img');
-    photo.src = bigPhotoUrl;
-    renderPhoto(photo)
-};
+  // Pobranie atrybutu "src" z element <img>
+  const currentUrl = this.src;
+  // Zamiana rozdzielczości obrazka
+  const bigPhotoUrl = currentUrl.replace("300", "900").replace("200", "600");
+  // Stworzenie elementu typu obrazek
+  const photo = document.createElement("img");
+  photo.src = bigPhotoUrl;
+  renderPhoto(photo);
+}
 
 images.forEach(function (image) {
-    //console.log("image z petli for each");
-    image.addEventListener("click", handleClickOnImage);
-})
+  // console.log('to jest "image" z pętli "forEach"', image);
+  image.addEventListener("click", handleClickOnImage);
+});
+
+// 1 images[0].addEventListener("click", handleClickOnImage);
+// 2 images[1].addEventListener("click", handleClickOnImage);
+// 3 images[2].addEventListener("click", handleClickOnImage);
