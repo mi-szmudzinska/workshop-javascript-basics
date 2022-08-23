7fd56b83e8f4436cefde8a063bd19f7ccc4cde43
Milena Szmudzinska <msa45@3ds.com>
2022-08-23 09:54:12 +0200

    Handle loop and delete photos

--- gallery/main.js
+++ gallery/main.js
@@ -1,21 +1,28 @@
-
-
 const images = document.querySelectorAll('img');
 console.log(images);
 
 const bigPhotoContainer = document.querySelector('#big-photo');
 
-images[0].addEventListener('click', function () {
+function renderPhoto(photo) {
+    //Usunięcie pierwszego dziecka z kontenera
+    bigPhotoContainer?.firstElementChild?.remove();
+    //Renderowanie obrazka 'photo' w elemncie 'bigPhotoContainer'\
+    bigPhotoContainer?.append(photo);
+}
+
+function handleClickOnImage() {
+    //Pobranie atrybutu src z elementu 
     const currentUrl = this.src;
-    const bigPhotoUrl = currentUrl
-        .replace('300', '900')
-        .replace('200', '600');
+    //Zamiana rozdzielczosci obrazka
+    const bigPhotoUrl = currentUrl.replace('300', '900').replace('200', '600');
     console.log(bigPhotoUrl)
     
     const photo = document.createElement('img');
     photo.src = bigPhotoUrl;
-    
-    bigPhotoContainer?.append(photo);
+    renderPhoto(photo)
+};
+
+images.forEach(function (image) {
+    //console.log("image z petli for each");
+    image.addEventListener("click", handleClickOnImage);
 })
-images[1] 
-images[2] 
